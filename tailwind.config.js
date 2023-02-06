fa2181587e3792ecdb52f2d507e105cf4b60d539
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-06 22:11:22 +0100

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        background: "#232946",
+      },
+      textColor: {
+        headline: "#fffffe",
+        paragraph: "#b8c1ec",
+        buttonText: "#232946",
+        illustrationStroke: "#121629",
+        main: "#b8c1ec",
+        highlight: "#eebbc3",
+        secondary: "#fffffe",
+        tertiary: "#eebbc3",
+      },
+      backgroundColor: {
+        button: "#eebbc3",
+      },
+    },
+  },
+  plugins: [],
+};
