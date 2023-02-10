72782bb865a553d429d5dad9183a1fed568302e4
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-10 09:19:37 +0100

    Added userList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
+import useGetUsers from "@/hooks/useGetUsers";
 import React, { FC } from "react";
 
 interface userListProps {}
 
 const UserList: FC<userListProps> = ({}) => {
+  const [response, error] = useGetUsers();
+
+  const users = response?.items;
+  console.log(users);
   return (
     <div className='sm:block hidden max-w-xs bg-background h-screen w-full overflow-y-scroll cursor-pointer'>
       {/* User */}
@@ -22,26 +26,24 @@ const UserList: FC<userListProps> = ({}) => {
       </div>
       {/* FriendList */}
       <div className=' bg-secondary flex flex-col divide-y '>
-        {Array(20)
-          .fill(0)
-          .map((_, index) => (
-            <div
-              key={index}
-              className='flex gap-2 p-2 hover:bg-tertiary cursor-pointer'>
-              <img
-                className='w-10 h-10 rounded-full'
-                src={
-                  "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=6&m=1223671392&s=612x612&w=0&h=NGxdexflb9EyQchqjQP0m6wYucJBYLfu46KCLNMHZYM="
-                }
-                alt={""}></img>
-              <div className='flex flex-col'>
-                <p className='text-sm font-semibold text-headline'>
-                  Rikard Johansson
-                </p>
-                <p className='text-xs text-paragraph'>I hate you...</p>
-              </div>
+        {users?.map((user, index) => (
+          <div
+            key={index}
+            className='flex gap-2 p-2 hover:bg-tertiary cursor-pointer'>
+            <img
+              className='w-10 h-10 rounded-full'
+              src={`${
+                user.avatar
+                  ? user.avatar
+                  : "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=6&m=1223671392&s=612x612&w=0&h=NGxdexflb9EyQchqjQP0m6wYucJBYLfu46KCLNMHZYM="
+              } `}
+              alt={""}></img>
+            <div className='flex flex-col'>
+              <p className='text-sm font-semibold text-headline'>{user.name}</p>
+              <p className='text-xs text-paragraph'>I hate you...</p>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
